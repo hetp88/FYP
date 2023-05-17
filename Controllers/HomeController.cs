671c84bbec81c2502b20a3aa372c302317ba896c
21024102 <21024102@PHOEBE>
2023-05-18 00:36:38 +0800

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using FYP.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FYP.Controllers
+{
+    public class HomeController : Controller
+    {
+        public String Index() => "HomePage";
+    }
+}
---
+++ Models/UserLogin.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FYP.Models
+{
+    public class UserLogin
+    {
+        [Required(ErrorMessage = "Please enter User ID")]
+        public string UserID { get; set; } = null!;
+
+        [Required(ErrorMessage = "Please enter Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+    }
+
+}
