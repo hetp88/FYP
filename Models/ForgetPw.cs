47b4b2e66b0aa376ffb4a663d6d93e9746fc4ef2
phoebe hung <hetp88@yahoo.com>
2023-05-24 16:56:10 +0800

    .

---
+++ Models/ForgetPw.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FYP.Models
+{
+    public class ForgetPw
+    {
+        [Required(ErrorMessage = "Please enter ypur email")]
+        public string Email { get; set; } = null!;
+    }
+}
--- Models/NewUser.cs
+++ Models/NewUser.cs
@@ -1,6 +1,31 @@
-﻿namespace FYP.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FYP.Models
 {
     public class NewUser
     {
+
+        [Required(ErrorMessage = "Please enter Password")]
+        [StringLength(20, MinimumLength = 8, ErrorMessage = "Password must be 8-20 char")]
+        [DataType(DataType.Password)]
+        public string UserPw { get; set; } = null!;
+
+        [Compare("UserPw", ErrorMessage = "Passwords do not match")]
+        [DataType(DataType.Password)]
+        public string UserPw2 { get; set; } = null!;
+
+        [Required(ErrorMessage = "Please enter Full Name")]
+        public string FullName { get; set; } = null!;
+
+        [Required(ErrorMessage = "Please choose school")]
+        public string School { get; set; } = null!;
+
+        [Required(ErrorMessage = "Please enter Email")]
+        [EmailAddress(ErrorMessage = "Invalid Email")]
+        public string Email { get; set; } = null!;
+
+        [Required(ErrorMessage = "Please enter Fitness Score")]
+        [RegularExpression("\\d[8]", ErrorMessage = "8 numbers only")]
+        public int PhoneNo { get; set; }
     }
 }
