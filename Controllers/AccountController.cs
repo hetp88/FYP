6590e1bda8c0520fdcb2380191514c152fa03eb9
YusufMazlan <21018634@myrp.edu.sg>
2023-05-24 17:21:15 +0800

    -

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,13 +1,50 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Security.Claims;
+using System;
+using FYP.Models;
 
-namespace FYP.Controllers
+namespace FYP.Controllers;
+public class AccountController : Controller
 {
-    public class AccountController : Controller
+    private const string LOGIN_SQ =
+        @"";
+    private const string LASTLOGIN_SQ =
+        @"";
+    private const string ForgetPW_SQ =
+        @"";
+    public IActionResult Login()
     {
-        public IActionResult Login()
-        {
-            return View();
-        }
+        return View();
+    }
+    public IActionResult Register()
+    {
+        return View("Register");
+    }
+    public IActionResult Register(NewUser usr)
+    {
+        return View();
+    }
+    private static bool AuthenticateUser(string uid, string pw, out ClaimsPrincipal principal)
+    {
+        principal = null!;
 
+        DataTable ds = DBUtl.GetTable(LOGIN_SQ, uid, pw);
+        if (ds.Rows.Count == 1)
+        {
+            principal =
+               new ClaimsPrincipal(
+                  new ClaimsIdentity(
+                     new Claim[] {
+                     new Claim(ClaimTypes.NameIdentifier, uid),
+                     }, "Basic"
+                  )
+               );
+            return true;
+        }
+        return false;
     }
 }
+
+
+
