600e5a5f961bd8b735597689d7941dce91ca9330
YusufMazlan <21018634@myrp.edu.sg>
2023-05-27 02:41:25 +0800

    -

--- Controllers/FaqController.cs
+++ Controllers/FaqController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FYP.Controllers
 {
@@ -8,6 +9,7 @@ namespace FYP.Controllers
         {
             return View();
         }
+        [Authorize(Roles = "admin, employee")]
         public IActionResult CreateFAQ()
         {
             return View();
