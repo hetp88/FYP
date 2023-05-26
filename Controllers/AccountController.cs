99e5de03c35093ce9e0d70dc0d516ad62b34c5b1
benjamintanweiren <https://github.com/benjamintanweiren>
2023-05-26 19:29:07 +0800

    im fucking done with fyp i want give up

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,22 +1,77 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Data;
 using System.Security.Claims;
-using System;
+using Microsoft.Data.SqlClient;
+using Dapper;
 using FYP.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 
 namespace FYP.Controllers;
 public class AccountController : Controller
 {
     private const string LOGIN_SQ =
-        @"";
+        @"SELECT * FROM users WHERE UserID = @userid AND Password = @user_pw";
+
     private const string LASTLOGIN_SQ =
-        @"";
+        @"UPDATE Users SET LastLogin = GETDATE() WHERE UserID = @UserID";
+
     private const string ForgetPW_SQ =
-        @"";
+        @"SELECT Password FROM Users WHERE UserID = @UserID AND Email = @Email";
+
+
+
+    private readonly IConfiguration _configuration;
+
+    public AccountController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    private string GetConnectionString()
+    {
+        return _configuration.GetConnectionString("DefaultConnection");
+    }
+
     public IActionResult Login()
     {
         return View();
     }
+
+    [HttpPost]
+    public IActionResult Login(UserLogin userLogin)
+    {
+        if (ModelState.IsValid)
+        {
+            string uid = userLogin.UserID;
+            string pw = userLogin.Password;
+
+            if (AuthenticateUser(uid, pw, out ClaimsPrincipal principal))
+            {
+                HttpContext.SignInAsync(principal); // Sign in the user
+
+                userLogin.RedirectToUsers = true; // Set the RedirectToUsers property to true
+
+                return RedirectToAction("/Account/Users"); // Redirect to the "Users" action in the "Account" controller
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid credentials");
+            }
+        }
+
+        return View(userLogin);
+    }
+
+    [Authorize]
+    public IActionResult Users()
+    {
+        // Retrieve user data and pass it to the view
+        DataTable usersData = DBUtl.GetTable(""); // Query to retrieve user data
+        return View(usersData);
+    }
+
+
     public IActionResult Register()
     {
         return View("Register");
@@ -48,8 +103,12 @@ public class AccountController : Controller
             return true;
         }
         return false;
+
+        
     }
 }
+    
+
 
 
 
--- Models/UserLogin.cs
+++ Models/UserLogin.cs
@@ -11,6 +11,7 @@ namespace FYP.Models
         [DataType(DataType.Password)]
         public string Password { get; set; } = null!;
         public bool RememberMe { get; set; }
+        public bool RedirectToUsers { get; set; }
     }
 
 }
