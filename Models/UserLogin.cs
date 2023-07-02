59595d9bae65f03129ba111dc7d055eb9776f3ba
phoebe hung <hetp88@yahoo.com>
2023-07-03 02:38:06 +0800

    .

--- Models/UserLogin.cs
+++ Models/UserLogin.cs
@@ -5,7 +5,6 @@ namespace FYP.Models
     public class UserLogin
     {
         [Required(ErrorMessage = "Please enter User ID")]
-        //[RegularExpression("(\\d{8}|\\d{4}|\\d{5}|\\d{3})", ErrorMessage = "either 3, 4, 5, 6 or 8 digits only")]
         public int UserID { get; set; }
 
         [Required(ErrorMessage = "Please enter Password")]
